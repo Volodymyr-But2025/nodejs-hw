b3272f2d91943cff33869be75094e31deb83788b
Volodymyr Butenko <voxa23ne@gmail.com>
2026-07-26 15:35:57 +0300

    dd

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -98,6 +98,7 @@ export const requestResetEmail = async (req, res) => {
     res.status(200).json({
       message: 'Password reset email sent successfully',
     });
+    return;
   }
 
   const token = jwt.sign(
